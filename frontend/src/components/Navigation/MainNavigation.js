0d5e10c1db2affb88064472cae754212c2305c3c
jshen1110 <jshen1110@users.noreply.github.com>
2020-12-03 14:31:47 -0800

    use token

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
 
+import AuthContext from  '../../context/auth-context';
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-    <header className="main-navigation">
-         <div className="main-navigation_logo">
-            <h1>ClinicConnect</h1>
-        </div>
-        <nav className ="main-navigation_items">
-            <ul>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/events">Events</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/bookings">Bookings</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/insurance">Insurance</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/auth">Authenticate</NavLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <AuthContext.Consumer>
+        {(context) => {
+            return (
+                <header className="main-navigation">
+                    <div className="main-navigation_logo">
+                        <h1>ClinicConnect</h1>
+                    </div>
+                    <nav className ="main-navigation_items">
+                        <ul>
+                            <li>
+                                <NavLink to="/about">About</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/events">Events</NavLink>
+                            </li>
+                            {context.token && (<li>
+                                <NavLink to="/bookings">Bookings</NavLink>
+                            </li>)}
+                            <li>
+                                <NavLink to="/insurance">Insurance</NavLink>
+                            </li>
+                            {!context.token && (<li>
+                                <NavLink to="/auth">Authenticate</NavLink>
+                            </li>)}
+                </ul>
+            </nav>
+        </header>
+            )
+        }}
+        
+    </AuthContext.Consumer>
+ 
 );
 
 export default mainNavigation;
