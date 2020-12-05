041a5e503bc73a220015d74ad0a4a0e95f9b782b
siyuchen2020 <chen.siyu2@husky.neu.edu>
2020-12-05 00:24:47 -0800

    create availability

--- frontend/src/pages/Availability.js
+++ frontend/src/pages/Availability.js
@@ -3,11 +3,22 @@ import './Availability.css';
 
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
-
+import AuthContext from '../context/auth-context';
 class AvailabilityPage extends Component{
     state = {
         creating: false
     };
+
+    static contextType = AuthContext;
+
+
+    constructor(props) {
+        super(props);
+        this.titleElRef = React.createRef();
+        this.descriptionElRef = React.createRef();
+        this.priceElRef = React.createRef();
+        this.dateElRef = React.createRef();
+    }
 
     startCreateAvailabilityHandler = () => {
         this.setState({creating: true});
@@ -15,6 +26,53 @@ class AvailabilityPage extends Component{
 
     modalConfirmHandler = () => {
         this.setState({creating: false});
+        const title = this.titleElRef.current.value;
+        const price = +this.priceElRef.current.value;
+        const date = this.dateElRef.current.value;
+        const description = this.descriptionElRef.current.value;
+
+        // validate input
+        if (title.trim().length ===0 || price < 0 || date.trim().length===0 || description.trim().length===0) {
+            return;
+        }
+        const newAvailability = {title, description, price, date};
+
+        const requestBody = {
+            query: `
+              mutation {
+                createAvailability(availabilityInput: {title: "${title}", description: "${description}", price:${price}, date:"${date}"}) {
+                    _id
+                    title
+                    description
+                    price
+                    date
+                }
+              }
+            `
+          };
+        //   console.log(JSON.stringify(requestBody));
+          const token = this.context.token;
+          fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token
+            }
+          })
+            .then(res => {
+              if (res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+              }
+              return res.json();
+            })
+            .then(resData => {
+              console.log(resData);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        
     };
     modalCancelHandler = () => {
         this.setState({creating: false});
@@ -22,6 +80,9 @@ class AvailabilityPage extends Component{
     fetchAvailability() {
         
     }
+
+
+    
     render(){
         return (
         <React.Fragment>
@@ -31,19 +92,19 @@ class AvailabilityPage extends Component{
                 <form>
                     <div className="form-control">
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title"></input>
+                        <input type="text" id="title" ref={this.titleElRef}></input>
                     </div>
                     <div className="form-control">
                         <label htmlFor="title">Description</label>
-                        <input type="text" id="description"></input>
+                        <input type="text" id="description" ref={this.descriptionElRef}></input>
                     </div>
                     <div className="form-control">
                         <label htmlFor="price">Price</label>
-                        <input type="number" id="title"></input>
+                        <input type="number" id="title" ref={this.priceElRef}></input>
                     </div>
                     <div className="form-control">
                         <label htmlFor="date">Date</label>
-                        <input type="date" id="date"></input>
+                        <input type="datetime-local" id="date" ref={this.dateElRef}></input>
                     </div>
                 </form>
             </Modal>}
