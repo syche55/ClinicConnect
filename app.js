43f590527cfe0ff9bd6d678329c1f34a40af2e8e
carrie016 <wang.xin9@husky.neu.edu>
2020-12-07 00:36:58 -0800

    footer email sending

--- app.js
+++ app.js
@@ -1,47 +1,102 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlHTTP } = require('express-graphql');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
-const isAuth = require('./middleware/is-auth');
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middleware/is-auth");
 
 const app = express();
+let nodemailer = require("nodemailer");
+const path = require("path");
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: "YOUREMAIL",
+    pass: "PASSWORDOFEMAIL",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+// verifying the connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages!");
+  }
+});
+
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => console.info(`server has started on ${PORT}`));
+
+// mailer ends
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if(req.method === 'OPTIONS') {
-        return res.sendStatus(200);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.post("/access", (req, res, next) => {
+  var email = req.body.email;
+  var message = req.body.message;
+  var content = `email: ${email} \n message: ${message} `;
+  console.log(content);
+
+  var mail = {
+    from: "fromEmail",
+    to: "destinationEmail",
+    subject: "CLINICCONNECT new inquiry",
+    text: content,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      console.log("no");
+      res.json({
+        status: "fail",
+      });
+    } else {
+      console.log("yes");
+      res.json({
+        status: "success",
+      });
     }
-    next();
+  });
 });
 
 app.use(isAuth);
 
 app.use(
-    '/graphql',
-    graphqlHTTP({
-        // when you call createAvailability(), you return an availability
-        schema: graphQlSchema,
-        rootValue: graphQlResolvers,
-        graphiql: true
-    })
+  "/graphql",
+  graphqlHTTP({
+    // when you call createAvailability(), you return an availability
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true,
+  })
 );
 
-mongoose.connect(`${process.env.MONGO_URL}`, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-              }).then(() => {
-            app.listen(8000);
-
-        }).catch(err => {
-            console.log(err);
-        });
-
-
+mongoose
+  .connect(`${process.env.MONGO_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,36 +1,68 @@
-import React from 'react';
-import './Footer.css';
-
+import React, { useState } from "react";
+import "./Footer.css";
 
 function Footer() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitRequest = async (e) => {
+    e.preventDefault();
+    console.log({ email, message });
+    const response = await fetch("http://localhost:8000/access", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ email, message }),
+    });
+
+    const resData = await response.json();
+    if (resData.status === "success") {
+      alert("Message Sent.");
+      // this.resetForm()
+      setEmail("");
+      setMessage("");
+    } else if (resData.status === "fail") {
+      alert("Message failed to send.");
+    }
+  };
+
   return (
-    <div className='footer-container'>
-      <section className='footer-contact'>
-        <p className='footer-contact-heading'>
-        2098 Walsh Ave Suite B, Santa Clara, CA 95050
+    <div className="footer-container">
+      <section className="footer-contact">
+        <p className="footer-contact-heading">
+          2098 Walsh Ave Suite B, Santa Clara, CA 95050
         </p>
         <div>
           <form className="footer-form">
             <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
             />
             <textarea
-              className='footer-info'
-              name='text'
-              type='text'
-              placeholder='Your Request'
+              className="footer-info"
+              name="message"
+              type="text"
+              placeholder="Your Request"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              required
             />
             <div className="header-btn">
-                <button type="button" type="submit">Contact  Us</button>
+              <button type="button" type="submit" onClick={submitRequest}>
+                Contact Us
+              </button>
             </div>
           </form>
         </div>
-        <div className='rights-inline'>
-          <small className='phone'>Tel: 123-456-7890</small>
-        <small className='website-rights'>ClinicConnect © 2020 </small>
+        <div className="rights-inline">
+          <small className="phone">Tel: 123-456-7890</small>
+          <small className="website-rights">ClinicConnect © 2020 </small>
         </div>
       </section>
     </div>
